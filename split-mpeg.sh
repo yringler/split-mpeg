6ff6a33271a486df8d5dd6d51518d9aa03aae5d4
Yehuda Ringler <guitarguy613@gmail.com>
2013-08-03 22:55:39 +0000

    stabilize

--- split-mpeg.sh
+++ split-mpeg.sh
@@ -14,18 +14,36 @@ cut_file=$2 # the file with the info on how to cut the video
 readonly out_folder=${3:-cut}	# the folder where the pieces are put
 if ! [ -d $out_folder ]; then mkdir $out_folder; fi
 
-function to_sec { echo $(( 60*$1+$2 )).${3:-0}; }
+function to_sec { echo $(( 60*$1+$2 )); }	# args:minute,second
+function calc_length {	# args:sec_a sec_b tenth_a tenth_b
+	diff_sec=$(( $2 - $1 ))
+	if [[ $4 && $3 ]]; then
+		if [ $4 -ge $a ]; then 
+			diff_tenth=$(( $4-$3 ))
+		else 
+			# eg 0.9->1.2 = ->1.0->1.1->1.2	= 0.3 = 1-0.9+0.2
+			diff_tenth=$(( 1-$3+$4 ))
+			let --diff_sec	# borrows from the seconds place
+		fi
+	elif [[ $4 || $3 ]]; then
+		diff_tenth=${4:-$3}
+	else
+		diff_tenth=0
+	fi
+
+	echo ${diff_sec}.${diff_tenth}
+}
 function extract {
 # having the tenths last makes them optional
 while read start_min start_sec end_min end_sec out_file start_tenth end_tenth
 do
-	start=$(to_sec $start_min $start_sec $start_tenth)
-	end=$(to_sec  $end_min $end_sec $end_tenth)
+	start_sec=$(to_sec $start_min $start_sec)
+	end_sec=$(to_sec  $end_min $end_sec)
 		## ffmpeg starts at offset, extracts duration ##
 		## but I use start and end so... ##
-	length=$(( end - start ))
+	length=$(calc_length $start_sec $end_sec $start_tenth $end_tenth)
 
-	ffmpeg -i $vid_file -ss $start -t $length \
+	ffmpeg -i $vid_file -ss $start_sec.$start_tenth -t $length \
 		-vcodec copy -acodec copy $out_folder/$out_file \
 		-loglevel error
 done
