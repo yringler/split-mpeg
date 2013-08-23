b3218dffebe7453537ce6ae87e813ed3723b15b6
Yehuda Ringler <guitarguy613@gmail.com>
2013-08-23 10:40:01 -0400

    very big, very messy commit. But it works now!

--- split-mpeg.sh
+++ split-mpeg.sh
@@ -14,15 +14,18 @@ readonly cut_file=${1:?error:info_file missing}
 readonly out_folder=${2:-cut}	# the folder where the pieces are put
 if ! [ -d $out_folder ]; then mkdir $out_folder; fi
 
-function to_sec { echo $(( 60*$1+$2 )); }	# args:minute,second
+function to_sec { echo $(( 60*${1?}+${2?} )); }	# args:minute,second
 
 function calc_length {	# args:sec_a tenth_a sec_b tenth_b
-	read sec_a tenth_a sec_b tenth_b <<< $(echo $*)
+	sec_a=${1?err:sec_a missing}
+	tenth_a=${2?err:tenth_a missing}
+	sec_b=${3?err:sec_b missing}
+	tenth_b=${4?err:tenth missing}
 
 	diff_sec=$(( $sec_b - $sec_a ))
 
 	if [ $tenth_b -ge $tenth_a ]; then 
-		diff_tenth=$(( $tenth_b-$tenth_a ))
+		diff_tenth=$(( tenth_b-tenth_a ))
 	else 
 		# eg 0.9->1.2 = ->1.0->1.1->1.2	= 0.3 = 1- 0.9 + 0.2
 		# has to be ten, b/c "tenth" is int
@@ -34,26 +37,19 @@ function calc_length {	# args:sec_a tenth_a sec_b tenth_b
 }
 
 function use_file {
-	if ! [ "$1" ]; then return; fi
+	if ! [ "$1" ]; then  echo asdf && return; fi
 
 	if [ $1 == load ]; then
-		if ! [ -e "$2" ]; then
-			echo $2:not found
-			exit
-		else
-			vid_file=$2
+			vid_file=${2?err: no vid file}
 			return
-		fi
-	fi
-
-	if [ $1 == c ]; then
-		start_sec=$end_sec
-		start_tenth=$end_tenth
+	elif [ $1 == c ]; then
+		start_sec=${end_sec?err: end_sec not set}
+		start_tenth=${end_tenth?err:end_tenth not set}
 		shift
 	else
 		# combine minutes and seconds
-		start_sec=$(to_sec $1 $2)
-		start_tenth=$3
+		start_sec=$(to_sec ${1?} ${2?})
+		start_tenth=${3?}
 		shift 3
 	fi
 
@@ -63,19 +59,23 @@ function use_file {
 	else
 		to_end=
 
-		end_sec=$(to_sec $1 $2)
-		end_tenth=$3
+		end_sec=$(to_sec ${1?} ${2?})
+		end_tenth=${3?}
 		shift 3
 	fi
 
-	out_file=$1
+	out_file=${1?}
+	if [ -e $out_folder/$out_file ]; then 
+		echo $out_file:exists
+		return
+	fi
 
 	if [ ! $to_end ]; then
 			## ffmpeg starts at offset, extracts duration	##
 			## but I use start and end so... 		##
-		length=$(calc_length $start_sec $start_tenth
+		length=$(calc_length $start_sec $start_tenth \
 			$end_sec $end_tenth)
-		length_section="-t $length"
+		length_section="-t ${length?}"
 	else
 		# so seeks to start, goes to end
 		length_section=
@@ -83,9 +83,10 @@ function use_file {
 
 	ffmpeg -i $vid_file -ss $start_sec.$start_tenth $length_section\
 		-vcodec copy -acodec copy $out_folder/$out_file \
-		-loglevel error
+		-loglevel error < /dev/null
 }
 
-cat $cut_file | while read line ; do
+while read line ; do
+	echo To process:$line:
 	use_file $line
-done
+done < $cut_file
